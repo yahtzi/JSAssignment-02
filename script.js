e4085d4fe4fbc67ecb4783b8b0e84b4931cf8887
yahtzi <yaritzabejarano7220@my.whccd.com>
2020-01-25 21:56:54 -0800

    I added lines 6 and 7

--- script.js
+++ script.js
@@ -16,13 +16,13 @@ console.log();
 console.log(3 * 8 + 2);
 // 5. Write code to output the result of the comparison:
 // 5 is less than 10.
-console.log(5 > 10);
+console.log(5 < 10);
 // 6. Write code to output the result of the comparison:
 // 10 is greater than 5.
-console.log();
+console.log(10 > 5);
 // 7. Write code to output the result of the comparison:
 // "Apples" is NOT equal to "Oranges"
-console.log();
+console.log("Apples" != "Oranges");
 // 8. Write code to output the result of the comparison:
 // 10 * 10 is greater than 50 AND 5 * 5 is less than 50
 console.log();
